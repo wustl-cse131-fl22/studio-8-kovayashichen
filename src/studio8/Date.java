479a683546b9660e65b6657f9b571630e21482be
faddy <faddy@localhost>
2022-11-17 13:56:32 -0600

    1

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,76 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
 
+	private String month = "";
+	private int day = 0;
+	private int year = 0;
 
-    public static void main(String[] args) {
+	/**
+	 * 
+	 * @param m month
+	 * @param d day
+	 * @param y year
+	 */
+	public Date(String m, int d, int y) {
+		this.month = m;
+		this.day = d;
+		this.year = y;
+	}
+	
 
-    }
+	public String toString() {
+		return month + " " +  day + " " + year;
+	}
+
+	public static void main(String[] args) {
+		Date date1 = new Date("Feb", 04, 2022);
+		System.out.println(date1);
+		
+		Date date2 = new Date("Feb", 05, 2022);
+		System.out.println(date1.equals(date2));
+		System.out.println(date1.hashCode());
+		System.out.println(date2.hashCode());
+		
+		LinkedList<Date> list = new LinkedList<Date>();
+		list.add(date1);
+		list.add(date2);
+		list.add(date1);
+		System.out.println(list);
+		System.out.println(list.hashCode());
+		
+		HashSet<Date> set = new HashSet<Date>();
+		set.add(date1);
+		set.add(date2);
+		set.add(date1);
+		System.out.println(set);
+		System.out.println(set.hashCode());
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && Objects.equals(month, other.month) && year == other.year;
+	}
+	
+	
 
 }
+
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,10 +2,28 @@ package studio8;
 
 
 public class Time {
+	private int hour = 0;
+	private int minute = 0;
 
+	/**
+	 * 
+	 * @param h hour
+	 * @param m minute
+	 */
+	public Time(int h, int m) {
+		this.hour=h;
+		this.minute=m;
+	}
+
+	public String toString() {
+		return hour + ":" + minute;
+	}
+	
 	public static void main(String[] args) {
- 
-    	
-    }
+		Time time1 = new Time(10, 15);
+		System.out.println(time1);
+		
+	}
+
 
 }
